ed50dbdcd6ebdc38eab6d55609d166b842312835
Polina Smirnova <smirnova.p.o@ya.ru>
2019-12-27 07:04:08 +0300

    fix primitive polynomials

--- bch/finitefield.py
+++ bch/finitefield.py
@@ -1,8 +1,6 @@
 from itertools import combinations
-import yaml
-import csv
 
-def get_primitive_polynomial(power, k):
+def get_primitive_polynomial(power):
     """
     Retrieves a table of primitive
     polynomials from the given in
@@ -12,37 +10,125 @@ def get_primitive_polynomial(power, k):
     
     :param power: the power of 2 in
     the length of a code (2ᵖᵒʷᵉʳ).
-    :param k: param in the table
-    corresponds to the size of the
-    polynomial.
-    Maps 1 -> 3, 2 -> 5, 3 -> 7
     
     :returns: a primitive polynomial
     in a binary representation.
     """
-    if power < 1 | power > 51:
-        raise ValueError('The parameter n should be 1 <= power <= 51, power is {0}'.format(power))
+    
+    prim_polynomials = {
+    2 : 0b111,
+    3 : 0b1011,
+    4 : 0b10011,
+    5 : 0b100101,
+    6 : 0b1000011,
+    7 : 0b10001001,
+    8 : 0b100011101,
+    9 : 0b1000010001,
+    10 : 0b10000001001,
+    11 : 0b100000000101,
+    12 : 0b1000001010011,
+    13 : 0b10000000011011,
+    14 : 0b100010001000011,
+    15 : 0b1000000000000011,
+    16 : 0b10001000000001011,
+    17 : 0b100000000000001001,
+    18 : 0b1000000000010000001,
+    19 : 0b10000000000000100111,
+    20 : 0b100000000000000001001,
+    # 21 : 0b x^21+x^2+1,
+    # 22 : 0b x^22+x+1,
+    # 23 : 0b x^23+x^5+1,
+    # 24 : 0b x^24+x^7+x^2+x+1,
+    # 25 : 0b x^25+x^3+1,
+    # 26 : 0b x^26+x^6+x^2+x+1,
+    # 27 : 0b x^27+x^5+x^2+x+1,
+    # 28 : 0b x^28+x^3+1,
+    # 29 : 0b x^29+x^2+1,
+    # 30 : 0b x^30+x^23+x^2+x+1,
+    # 31 : 0b x^31+x^3+1,
+    # 32 : x^32+x^22+x^2+x+1,
+    # 33 : x^33+x^13+1,
+    # 34 : x^34+x^8+x^4+x^3+1,
+    # 35 : x^35+x^2+1,
+    # 36 : x^36+x^11+1,
+    # 37 : x^37+x^6+x^4+x+1,
+    # 38 : x^38+x^6+x^5+x+1,
+    # 39 : x^39+x^4+1,
+    # 40 : x^40+x^5+x^4+x^3+1,
+    # 41 : x^41+x^3+1,
+    # 42 : x^42+x^7+x^4+x^3+1,
+    # 43 : x^43+x^6+x^4+x^3+1,
+    # 44 : x^44+x^6+x^5+x^2+1,
+    # 45 : x^45+x^4+x^3+x+1,
+    # 46 : x^46+x^8+x^7+x^6+1,
+    # 47 : x^47+x^5+1,
+    # 48 : x^48+x^9+x^7+x^4+1,
+    # 49 : x^49+x^9+1,
+    # 50 : x^50+x^4+x^3+x^2+1,
+    # 51 : x^51+x^6+x^3+x+1,
+    # 52 : x^52+x^3+1,
+    # 53 : x^53+x^6+x^2+x+1,
+    # 54 : x^54+x^8+x^6+x^3+1,
+    # 55 : x^55+x^24+1,
+    # 56 : x^56+x^7+x^4+x^2+1,
+    # 57 : x^57+x^7+1,
+    # 58 : x^58+x^19+1,
+    # 59 : x^59+x^7+x^4+x^2+1,
+    # 60 : x^60+x+1,
+    # 61 : x^61+x^5+x^2+x+1,
+    # 62 : x^62+x^6+x^5+x^3+1,
+    # 63 : x^63+x+1,
+    # 64 : x^64+x^4+x^3+x+1,
+    # 65 : x^65+x^18+1,
+    # 66 : x^66+x^9+x^8+x^6+1,
+    # 67 : x^67+x^5+x^2+x+1,
+    # 68 : x^68+x^9+1,
+    # 69 : x^69+x^6+x^5+x^2+1,
+    # 70 : x^70+x^5+x^3+x+1,
+    # 71 : x^71+x^6+1,
+    # 72 : x^72+x^10+x^9+x^3+1,
+    # 73 : x^73+x^25+1,
+    # 74 : x^74+x^7+x^4+x^3+1,
+    # 75 : x^75+x^6+x^3+x+1,
+    # 76 : x^76+x^5+x^4+x^2+1,
+    # 77 : x^77+x^6+x^5+x^2+1,
+    # 78 : x^78+x^7+x^2+x+1,
+    # 79 : x^79+x^9+1,
+    # 80 : x^80+x^9+x^4+x^2+1,
+    # 81 : x^81+x^4+1,
+    # 82 : x^82+x^9+x^6+x^4+1,
+    # 83 : x^83+x^7+x^4+x^2+1,
+    # 84 : x^84+x^13+1,
+    # 85 : x^85+x^8+x^2+x+1,
+    # 86 : x^86+x^6+x^5+x^2+1,
+    # 87 : x^87+x^13+1,
+    # 88 : x^88+x^11+x^9+x^8+,
+    # 89 : x^89+x^38+1,
+    # 90 : x^90+x^5+x^3+x^2+1,
+    # 91 : x^91+x^8+x^5+x+1,
+    # 92 : x^92+x^6+x^5+x^2+1,
+    # 93 : x^93+x^2+1,
+    # 94 : x^94+x^21+1,
+    # 95 : x^95+x^11+1,
+    # 96 : x^96+x^10+x^9+x^6+1,
+    # 97 : x^97+x^6+1,
+    # 98 : x^98+x^11+1,
+    # 99 : x^99+x^7+x^5+x^4+1,
+    # 100 : x^100+x^37+1,
+    # 101 : x^101+x^7+x^6+x+1,
+    # 102 : x^102+x^6+x^5+x^3+1,
+    # 103 : x^103+x^9+1,
+    # 104 : x^104+x^11+x^10+x+1,
+    # 105 : x^105+x^16+1,
+    # 106 : x^106+x^15+1,
+    # 107 : x^107+x^9+x^7+x^4+1,
+    # 108 : x^108+x^31+1,
+    # 109 : x^109+x^5+x^4+x^2+1,
+    # 110 : x^110+x^6+x^4+x+1,
+    # 111 : x^111+x^10+1
+    }
 
-    if k < 1 | k > 3:
-        raise ValueError('The parameter k should be 1 <= k <= 3, k is {0}'.format(k))
-
-    config = yaml.safe_load(open("config.yml"))
-    dictionary = {1: 1, 2: 2, 3: 5, 4: 10}
-    with open(config['primitive-polynomials'], newline='') as csvfile:
-        
-        primitive_polynomials = csv.reader(csvfile, delimiter=',')
-        for row in primitive_polynomials:
-
-            ints = list(map(lambda x: 0 if x == '' else int(x), row))
-            if ints[0] == power:
-                polynomial_powers = ints[dictionary[k]:dictionary[k + 1]]
-                polynomial_binary = 1 << power
-                polynomial_binary |= 1
-
-                for i in polynomial_powers:
-                    polynomial_binary |= 1 << i
-
-                return polynomial_binary
+    return prim_polynomials[power]
 
 def get_logarithm_table(power, primitive_polynomial):
     """
